191688a7fa8dcea6acc632c0d59aaa07e041b479
Steve Wu <hwu1986@gmail.com>
2012-05-19 22:43:46 -0400

    Split into header and footer; home page changes

---
+++ template/footer.php
@@ -0,0 +1,11 @@
+<!-- This is where the Footer Starts -->
+
+<div class="clear"></div>
+<div class="footer center">
+	<center>Copyright © 2012 San Fa Construction Supply. All rights reserved.<br>
+	Powered by: <a href="https://twitter.com/#!/HsienTsung_Wu">@HsienTsung_Wu</a></center>
+</div>
+	
+	
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,32 @@
+<!-- This is where the Header starts -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<title>San Fa Construction Supply</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+	<script src="js/home.js" type="text/javascript"></script>
+	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/jquery.lightbox-0.5.js" type="text/javascript"></script>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+	<link href="css/jquery.lightbox-0.5.css" rel="stylesheet" type="text/css">
+</head>
+<body class="center">
+	
+	<!-- Logo Placeholder -->
+	<div class="header">
+		<img id="logo" class="center" src="images/sanfa_logo.png" height="200px" width="400px"/>
+	</div>
+	
+	<!-- Navigation Placeholder -->
+	<div class="nav">
+		<ul>
+			<li><a id="home" href="#">Home</a></li>
+			<li><a id="products" href="#">Products</a></li>
+			<li><a id="promotions" href="#">Promotions</a></li>
+			<li><a id="aboutus" href="#">About Us</a></li>
+			<li><a id="contactus" href="#">Contact Us</a></li>
+		</ul>
+	</div>
+	
+	<div class="clear"></div>
